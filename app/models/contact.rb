2de060aa4a3b98015733b8c22de12977b4927e0c
Diego Stiehl <seccomiro@gmail.com>
2019-10-09 23:16:36 -0300

    Add model associoations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   belongs_to :company
+  has_many :phones
+  has_one :address
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :contacts
+  
+  has_and_belongs_to_many :contacts
 end
