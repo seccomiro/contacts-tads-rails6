300440f82e1573435c5e737e7153fb407e87b610
Diego Stiehl <seccomiro@gmail.com>
2019-10-16 23:27:59 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_user email = nil, password = nil
+  password = password || Faker::Crypto.md5
+  user = User.create! email: email || Faker::Internet.email, password: password, password_confirmation: password
+  5.times do
+    user.contacts << Contact.all.sample
+  end
+end
+
+5.times do
+  Kind.create! description: Faker::Company.unique.industry
+end
+
+10.times do
+  company = Company.create! name: Faker::Company.unique.name, active: Faker::Boolean.boolean
+  10.times do
+    contact = company.contacts.create! name: Faker::Movies::StarWars.character, email: Faker::Internet.email, remark: Faker::Company.catch_phrase, kind: Kind.all.sample
+
+    contact.create_address! city: Faker::Address.city, street: Faker::Address.street_name, state: Faker::Address.state
+    
+    3.times do
+      contact.phones.create! number: Faker::PhoneNumber.cell_phone
+    end
+  end
+end
+
+20.times do
+  create_user
+end
+create_user 'test@test.com', '123123'
